3efd0bc6ded9482043badc9d348c2dfd26867ef4
Avneesh-123 <avitanwar6443@gmail.com>
2022-07-20 22:03:00 +0530

    commited

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.get('/',(req,res)=>{
 
 const PORT = process.env || 8000;
 
-mongoose.connect(process.env.CONNECTION_URL||  "mongodb+srv://MemoriesApp:MemoriesApp123@cluster0.d7wjqhn.mongodb.net/?retryWrites=true&w=majority" , {useNewUrlParser: true})
+mongoose.connect(process.env||  "mongodb+srv://MemoriesApp:MemoriesApp123@cluster0.d7wjqhn.mongodb.net/?retryWrites=true&w=majority" , {useNewUrlParser: true})
     .then(() => 
     {
         console.log("connected");
